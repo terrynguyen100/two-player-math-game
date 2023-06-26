8963edb8d923ecae762f556f20ebce80ae6efb45
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-26 16:12:38 -0600

    add instance method play

--- MathGame.rb
+++ MathGame.rb
@@ -34,10 +34,19 @@ class MathGame
         puts "Player #{@current_player + 1}: YES! You are correct"
       else
         puts "Player #{@current_player + 1}: NO! You are wrong"
+        @players[@current_player].lose_life
+
+      end
+      puts "P1: #{@players[0].lives}/3 vs. P2: #{@players[1].lives}/3"
+
+      switch_player
+      if game_over
+        puts ""
+        puts "Player #{@current_player + 1} wins with a score of #{@players[@current_player].lives}/3"
+        puts "----- GAME OVER -----"
+        puts "Good bye!"
       end
       
-      switch_player
-      @players[@current_player].lose_life
     end
   end
 end
